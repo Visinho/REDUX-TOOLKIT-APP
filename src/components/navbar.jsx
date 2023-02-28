43e2a52bc2dd9e9c02884f4b0290227e8b145ab5
Visinho <Elvisvisinho@rocketmail.com>
2023-02-28 10:05:16 +0100

    Set up the store and the cart reducer

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { CartIcon} from '../icons';
+import { useSelector } from 'react-redux';
+
+const navbar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default navbar
---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from "./redux/cart/cartSlice";
+
+
+export const store = configureStore({
+    reducer: {
+        cart: cartReducer,
+    },
+  });
+ 
