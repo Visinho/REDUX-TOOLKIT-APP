41a8855c6d26c3207ba914b2268cac380b106cd7
Visinho <Elvisvisinho@rocketmail.com>
2023-02-28 20:13:02 +0100

    Design of the cart component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,12 +2,25 @@ import React from 'react'
 import { CartIcon} from '../icons';
 import { useSelector } from 'react-redux';
 
-const navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const Navbar = () => {
+ 
+const { amount } = useSelector((state) => state.cart);
 
-export default navbar
+  return (
+    <nav>
+        <div className='nav-center'>
+            <h3>Visinho Toolkit</h3>
+            <div className='nav-container'>
+                <CartIcon />
+                <div className="amount-container">
+                    <p className='total-amount'>
+                        {amount}
+                    </p>
+                </div>
+            </div>
+        </div>
+    </nav>
+  );
+};
+
+export default Navbar
