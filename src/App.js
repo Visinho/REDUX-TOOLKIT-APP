cac25cc41655409f5b4c127825121ff7ff300df7
Visinho <Elvisvisinho@rocketmail.com>
2023-03-02 03:33:15 +0100

    implementation of the total price and total quantity functions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
+import { useDispatch, useSelector } from "react-redux";
+import { calculateTotals } from "./redux/cart/cartSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const { cartItems } = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
   return (
     <main>
       <Navbar/>
