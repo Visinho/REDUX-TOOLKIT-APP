73308950cccab108ecf0fd0733d5b8026347f1dd
Visinho <Elvisvisinho@rocketmail.com>
2023-03-02 04:16:36 +0100

    design of modal for confirmation or delete item

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from "./redux/cart/cartSlice";
+import modalReducer from "./redux/modal/modalSlice";
 
 
 export const store = configureStore({
     reducer: {
         cart: cartReducer,
+        modal: modalReducer,
     },
   });
  
