d01fd17e1666e276206486812c8956b820316fef
Visinho <Elvisvisinho@rocketmail.com>
2023-03-03 02:03:50 +0100

    Fetched data using AsyncThunk

--- src/App.js
+++ src/App.js
@@ -2,17 +2,30 @@ import { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 import { useDispatch, useSelector } from "react-redux";
-import { calculateTotals } from "./redux/cart/cartSlice";
+import { calculateTotals, getCartItems } from "./redux/cart/cartSlice";
 import Modal from "./components/Modal";
 
 function App() {
   const dispatch = useDispatch();
-  const { cartItems } = useSelector((state) => state.cart);
+  const { cartItems, isLoading } = useSelector((state) => state.cart);
   const { isOpen } = useSelector((state) => state.modal);
 
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
+if(isLoading){
+  return (
+    <div className="loading">
+      <h1>Loading...</h1>
+    </div>
+  )
+}
+
   return (
     <main>
       {isOpen && <Modal />}
